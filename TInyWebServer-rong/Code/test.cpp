b7bb7d045e4b39ce6d8375941580937e6a14fa2c
Rong0205 <1565011336@qq.com>
2024-12-16 14:14:48 +0800

    测试文件

--- TInyWebServer-rong/Code/test.cpp
+++ TInyWebServer-rong/Code/test.cpp
@@ -1,18 +1,41 @@
 #include "conpool.h"
+#include "threadpool.h"
 
-int main(){
-    
-    clock_t start = clock();
-
-    ConnectionPool* connpool = ConnectionPool::getConnectionPool("127.0.0.1", 3306, "root", "20010205", "test", 10, 1024, 60, 100);
-    for(int i = 0; i<1000; i++){
+void testDatabaseQuery(ConnectionPool* connpool) {
+    for(int i = 0; i<2000; i++){
         std::string sql = "insert into example_table(name, age) values('zhangsan', 18);";
         std::shared_ptr<Connection> conn = connpool->getConnection();
         conn->update(sql);
     }
-    clock_t end = clock();
+    std::cout << "insert success" << std::endl;
+}
+int main(){
 
-    std::cout << "cost time: " << double(end - start)/CLOCKS_PER_SEC*1000 << std::endl;
-    std::cout << "hello world" << std::endl;
-    _Exit(0);
+    ThreadPool* thPool = ThreadPool::getInstance(4);
+    ConnectionPool* connPool = ConnectionPool::getConnectionPool("127.0.0.1", 3306, "root", "20010205", "test", 10, 1024, 10, 100);
+    for(int i = 0; i<10; i++){
+        thPool->AddTask(testDatabaseQuery, connPool);
+    }
+
+    //线程池测试
+    // std::cout<<"hello world"<<std::endl;
+    // ThreadPool* pool = ThreadPool::getInstance(4);
+    // for(int i = 0; i< 10; i++){
+    //     pool->AddTask([](){std::cout<<"hello world"<<std::endl;});
+    // }
+
+    //连接池单独测试 
+    // clock_t start = clock();
+
+    // ConnectionPool* connpool = ConnectionPool::getConnectionPool("127.0.0.1", 3306, "root", "20010205", "test", 10, 1024, 10, 10);
+    // for(int i = 0; i<1000; i++){
+    //     std::string sql = "insert into example_table(name, age) values('zhangsan', 18);";
+    //     std::shared_ptr<Connection> conn = connpool->getConnection();
+    //     conn->update(sql);
+    // }
+    // clock_t end = clock();
+
+    // std::cout << "cost time: " << double(end - start)/CLOCKS_PER_SEC*1000 << std::endl;
+    // std::cout << "hello world" << std::endl;
+    return 0;
 }
