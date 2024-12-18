4c99f9b95b6224cfed267ea8f7086cbfaccd3d88
Rong0205 <1565011336@qq.com>
2024-12-18 22:14:56 +0800

    测试文件

--- TInyWebServer-rong/Code/test.cpp
+++ TInyWebServer-rong/Code/test.cpp
@@ -7,14 +7,21 @@ int main(){
     Buffer buff(1024);
     ConnectionPool* connPool = ConnectionPool::getConnectionPool("127.0.0.1", 3306, "root", "20010205", "test", 10, 2048, 10, 100);
     auto conn = connPool->getConnection();
-    int cnt = 0; 
+    //int cnt = 0; 
     int level = 0;
     Log::Instance()->init(level, "./testlog", ".log", 0);
-    LOG_BASE(cnt++,"aaaa");
-    bool istrue = HttpRequest::UserVerifyLogin("rongziran4", "12345", connPool);
-    if(istrue) std::cout <<"yes"<<std::endl;
-    else std::cout <<"no"<<std::endl;
-    //getchar();
+
+    
+    char testStr[] = "GET / HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/80.0.398";
+    buff.Append(testStr, sizeof(testStr));
+    std::cout << sizeof(testStr) << std::endl;
+    std::cout << buff.ReadableBytes() << std::endl;
+
+    HttpRequest request;
+    request.parse(buff, connPool);
+
+
+
     
     return 0;
 }
